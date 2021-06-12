00760b0c500a681d241ad11aa01d6012e06701c4
gapinska <gapinska.mon@gmail.com>
2021-06-12 21:41:41 +0200

    refactor

--- src/useContextHook.js
+++ src/useContextHook.js
@@ -2,12 +2,12 @@ import React, { useContext, useState } from "react"
 
 const themes = {
   light: {
-    foreground: "#000000",
-    background: "#eeeeee",
+    color: "#000000",
+    backgroundColor: "#eeeeee",
   },
   dark: {
-    foreground: "#ffffff",
-    background: "#222222",
+    color: "#ffffff",
+    backgroundColor: "#222222",
   },
 }
 
